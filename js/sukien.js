e15868146400f96039fb0500169a0a984a7ae2df
DinhDat06 <dat681120@gmail.com>
2026-06-06 13:50:03 +0700

    load lại trang

--- js/sukien.js
+++ js/sukien.js
@@ -1,28 +1,20 @@
 $(document).ready(function(){
 
-    // Gọi đúng tên hàm khởi tạo dữ liệu
     initData();
 
-    // Vẽ bảng danh sách lần đầu khi mở trang web
     renderTable();
 
-    // Sự kiện khi bấm nút "Thêm Phiếu Mượn"
     $("#btnThem").click(function(){
-        // Xóa sạch dữ liệu cũ trong form
-        $("#borrowForm")[0].reset();
-        
-        // Reset trình duyệt xóa các thông báo lỗi Validate đỏ của lần nhập trước (nếu có)
-        $("#borrowForm").validate().resetForm();
 
-        // Đặt ô ẩn editIndex về rỗng để hệ thống hiểu là đang "Thêm mới"
+        $("#borrowForm")[0].reset();
+
         $("#editIndex").val("");
 
-        // Hiển thị Form Popup lên màn hình
-        $("#borrowModal").css("display", "flex");
+        $("#borrowModal").css("display","flex");
     });
 
-    // Sự kiện bấm nút "Hủy" hoặc nút đóng Form
     $("#btnClose").click(function(){
+
         $("#borrowModal").hide();
     });
 
